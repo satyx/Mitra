a4482c3d4bb4e1f7ab1267e2799303c4844a2afe
satyx <satyamcrj@gmail.com>
2019-11-06 15:19:56 +0530

    Client Activity Monitoring: Console Display

--- server.py
+++ server.py
@@ -7,6 +7,18 @@ import json
 #userbase = ["Satyam","Rohit","Ashay","Jalaj","Shruti","Biswajit","Mansi"]
 with open("userbase.json") as db:
     userbase = json.load(db)
+#HOST = '10.145.233.242'
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+clients = {}
+addresses = {}
+
+if len(sys.argv)!=3:
+    print("Usage:python <filename> <host> <port>")
+    sys.exit(1)
 
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
@@ -23,17 +35,17 @@ def accept_incoming_connections():
     finally:
         return
 
-def authentication(client):
+def authentication(client):         # Returns <credentials' validity> <Retry?> <Username(=None, when invalid)>
     broadcast_selective(bytes("Enter Your Username", "utf8"),[client])
     username = client.recv(BUFSIZ).decode("utf8")
-    print(username,"yess")
     if not username:
         client.close()
         return False,False,None
     elif username == "{quit}":
         broadcast_selective(bytes("{quit}", "utf8"),[client])
         return False,False,None
-        
+       
+    print("<%s> is attempting to login" %username)
     broadcast_selective(bytes("Enter Password","utf-8"),[client])
     password = client.recv(BUFSIZ).decode("utf8")
     if not password:
@@ -45,8 +57,9 @@ def authentication(client):
     
     if username not in userbase or password != userbase[username]:
         broadcast_selective(bytes("Invalid Username or Password", "utf8"),[client])
+        print("<%s> Invalid Username/Password" %username)
         return False,True,None
-        
+    print("<%s> successfully logged in" %username)
     return True,False,username
 
 
@@ -57,17 +70,18 @@ def handle_client(client,client_address):  # Takes client socket as argument.
         valid, retry, username = authentication(client)
         if valid:
             break
+        
         if not retry:
             broadcast_selective(bytes("{quit}", "utf8"),[client])
             #client.send(bytes("{quit}", "utf8"))
             print("%s:%s has disconnected." % client_address)
             client.close()
             return
-    
+
     welcome = 'Welcome ! If you ever want to quit, type {quit} to exit.'
     broadcast_selective(bytes(welcome, "utf8"),[client])
     msg = "%s has joined the chat!" % username
-    broadcast(bytes(msg, "utf8"),client_address)
+    broadcast_global(bytes(msg, "utf8"),client_address)
     clients[client] = username
 
     while True:
@@ -78,30 +92,31 @@ def handle_client(client,client_address):  # Takes client socket as argument.
             del clients[client]
             break
         elif msg != bytes("{quit}", "utf8"):
-            broadcast(msg,client_address, username+": ")
+            broadcast_global(msg,client_address, username+": ")
         else:
             broadcast_selective(bytes("{quit}", "utf8"),[client])
             print("%s:%s has disconnected." % client_address)
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % username, "utf8"),client_address)
+            broadcast_global(bytes("%s has left the chat." % username, "utf8"),client_address)
             break
 
 
-def broadcast(msg,client_address=None, prefix=""):  # prefix is for name identification.
+def broadcast_global(msg,client_address=None, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
     invalid_clients=[]
-    for sock in clients:
+    for client in clients:
         try:
-            sock.send(bytes(prefix, "utf8")+msg)
+            client.send(bytes(prefix, "utf8")+msg)
         except BrokenPipeError:
-            invalid_clients.append(sock)
+            invalid_clients.append(client)
             continue
     for client in invalid_clients:
         print("%s:%s has disconnected." % client_address)
         client.close()
         del clients[client]
-        broadcast(bytes("%s has left the chat." % name, "utf8"))
+        broadcast_global(bytes("%s has left the chat." % name, "utf8"))
+
 
 def broadcast_selective(msg,client_list):
     invalid_clients=[]
@@ -109,36 +124,23 @@ def broadcast_selective(msg,client_list):
         try:
             (client_list[index]).send(msg)
         except BrokenPipeError or OSError:
-            invalid_clients.append(sock)
+            invalid_clients.append(client_list[index])
             continue
     for client in invalid_clients:
         print("%s:%s has disconnected." % client_address)
         client.close()
         del clients[client]
-        broadcast(bytes("%s has left the chat." % name, "utf8"))
+        broadcast_global(bytes("%s has left the chat." % name, "utf8"))
 
-        
-clients = {}
-addresses = {}
-
-if len(sys.argv)!=3:
-    print("Usage:python <filename> <host> <port>")
-    sys.exit(1)
-#HOST = '10.145.233.242'
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-
-SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SERVER.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-SERVER.bind(ADDR)
 
 if __name__ == "__main__":
     try:
+        SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        SERVER.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)        #To override, if the given address is already in use
+        SERVER.bind(ADDR)
         SERVER.listen(5)
         print("Waiting for connection...")
-        ACCEPT_THREAD = Thread(target=accept_incoming_connections,daemon=True)
+        ACCEPT_THREAD = Thread(target=accept_incoming_connections,daemon=True) #Stop execution of ACCEPT_THREAD as soon as server terminates
         print("-----Enter {quit} to exit-----")
         ACCEPT_THREAD.start()
         while True:
